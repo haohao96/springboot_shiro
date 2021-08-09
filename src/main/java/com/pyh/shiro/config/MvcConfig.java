62d405a6c9d648efa6ac4b68b4804bee9ce83723
xrzs <18437927639@163.com>
2021-08-09 17:30:13 +0800

    swagger

--- src/main/java/com/pyh/shiro/config/MvcConfig.java
+++ src/main/java/com/pyh/shiro/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.pyh.shiro.config;
 
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,4 +17,15 @@ public class MvcConfig implements WebMvcConfigurer {
 //        registry.addViewController("/select").setViewName("select");
 
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
--- src/main/java/com/pyh/shiro/controller/MyController.java
+++ src/main/java/com/pyh/shiro/controller/MyController.java
@@ -2,6 +2,7 @@ package com.pyh.shiro.controller;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,16 @@ public class MyController {
         Subject subject = SecurityUtils.getSubject();
 
         subject.login(usernamePasswordToken);
+        Session session = subject.getSession();
+        session.setAttribute("loginuser",usernamePasswordToken);
         return "suc";
+    }
+
+    @RequestMapping("/logout")
+    public String logout()
+    {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return "index";
     }
 }
