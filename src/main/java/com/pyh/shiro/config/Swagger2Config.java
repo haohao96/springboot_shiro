b72433cf9ea8d89fdcdaf12606c684988ed178bd
心若止水 <18437927639@163.com>
2021-08-09 23:42:50 +0800

    swagger Config sucess

--- src/main/java/com/pyh/shiro/config/Swagger2Config.java
+++ src/main/java/com/pyh/shiro/config/Swagger2Config.java
@@ -1,9 +1,40 @@
 package com.pyh.shiro.config;
 
+import com.alibaba.druid.sql.visitor.functions.Concat;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
+    @Bean
+    public Docket docket()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo()
+    {
+        Contact contact = new Contact("皮一豪","pyh.com","18437927639@163.com");
+        return new ApiInfo(
+                "皮一豪的Swagger",
+                "Api Documentation",
+                "1.0",
+                "urn:tos",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+    }
 }
